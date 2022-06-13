a76d9c3289d29123d18aa391ffa1935c52252ff4
Prince Codes <princecodes247@gmail.com>
2022-06-13 11:41:29 -0400

    Create contact form Component

---
+++ components/ContactForm.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <form action="">
+    <input type="text" placeholder="Name">
+    <input type="text" placeholder="Email">
+    <input type="text" placeholder="Subject">
+    <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+  </form>
+</template>
+
