09d586228934c908c8837fdfe1f7ea19c6859550
Prince Codes <princecodes247@gmail.com>
2022-06-14 09:06:58 -0400

    Create Mouse tracking component

---
+++ components/MouseTracker.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    update(e) {
+      this.x = e.pageX;
+      this.y = e.pageY;
+    },
+  },
+  mounted() {
+    window.addEventListener("mousemove", this.update);
+  },
+  unmounted() {
+    window.removeEventListener("mousemove", this.update);
+  },
+};
+</script>
+
+<template>
+  <div>
+    <slot :x="x" :y="y" />
+  </div>
+</template>
